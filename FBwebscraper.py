7e720154105224dfbb7bb2bb7cb3111fcc77fbe6
davidrodgers2002 <80054870+davidrodgers2002@users.noreply.github.com>
2023-11-17 11:45:44 +0000

    Cleaned up JSON output
    There's a slim possibility I may have broken the whole project. 
    
    sowwy 👉👈

--- FBwebscraper.py
+++ FBwebscraper.py
@@ -22,6 +22,33 @@ headless = False
 pagecounter = 0
 page = page_list[pagecounter]
 
+#--David's Ugly JSON duckery--
+#There's probably a more efficient way to do this than making a function for each output but I just want it working for now, sue me
+def get_json_name(input):
+    start = input.find("{\"name\": ")+10
+    input = input[start:len(input)]
+    end = input.find('",')
+    return input[0:end] 
+
+def get_json_shares(input):
+    start = input.find("\"shares\": ")+10
+    input = input[start:len(input)]
+    end = input.find(',')
+    return input[0:end]
+
+def get_json_comments(input):
+    start = input.find("\"comments\": ")+12
+    input = input[start:len(input)]
+    end = input.find(',')
+    return input[0:end]
+
+def get_json_content(input):
+    start = input.find("\"content\": \"")+10
+    input = input[start:len(input)]
+    end = input.find('\", ')
+    return input[0:end] + "\"" 
+#--
+
 for page in page_list:
     #authentication for smartproxy
     proxy = f'username:password@us.smartproxy.com:{proxy_port}'
@@ -31,7 +58,10 @@ for page in page_list:
     
     #printing the json data
     json_data = scraper.scrap_to_json()
-    print(json_data)
+    print("name: " + get_json_name(json_data))
+    print("content: " + get_json_content(json_data))
+    print("shares: " + get_json_shares(json_data))
+    print("comments: " + get_json_comments(json_data))
     
     #change proxy port to avoid facebook's blocking
     proxy_port += 1
